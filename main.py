ab8ac159438a8a0861c25592a7cd3548c07466f6
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2023-11-25 21:13:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import requests
 
 
 def call_api():
-    res = requests.get(url="https://feedback-json-data-server.onrender.com/")
-    print(f"Response from API: {res.status_code}")
+    try:
+        res = requests.get(url="https://feedback-json-data-server.onrender.com/")
+        print(f"Response from API: {res.status_code}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
 
 call_api()
