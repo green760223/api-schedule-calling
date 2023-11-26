d31fb8bba22fdb3473ea44d99acefecc6fe99df5
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2023-11-25 21:07:29 -0800

    Update files

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import requests
-import schedule
-import time
 
 
 def call_api():
@@ -8,9 +6,5 @@ def call_api():
     print(f"Response from API: {res.status_code}")
 
 
-schedule.every(1).minutes.do(call_api)
+call_api()
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(2)
-#     print("sleeping...")
